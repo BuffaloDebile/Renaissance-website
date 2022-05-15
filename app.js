95dbee6fb303cc52e1661d173b8f0414c600d5da
Paul-Adrien <pauladrienbourgue@gmail.com>
2022-05-15 16:25:41 +0200

    adding cursor animation

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 let controller;
 let slideScene;
 
+gsap.registerPlugin(ScrollTrigger);
+
 const slides = document.querySelectorAll(".slide");
 const nav = document.querySelector(".nav-header");
+let mouse = document.querySelector(".cursor");
+let mouseTxt = mouse.querySelector("span");
 
-slides.forEach((slide) => {
+
+slides.forEach((slide, index, slides) => {
   const img = slide.querySelector("img");
   const imgReveal = slide.querySelector(".reveal-img");
   const textReaveal = slide.querySelector(".reveal-text");
@@ -17,7 +22,7 @@ slides.forEach((slide) => {
     scrollTrigger: {
       trigger: slide,
       start: "top center",
-      markers: true,
+
       toggleActions: "play none none none",
     },
   });
@@ -27,10 +32,71 @@ slides.forEach((slide) => {
     .fromTo(textReaveal, { x: "0%" }, { x: "100%" }, "-=0.75")
     .fromTo(nav, { y: "-100%" }, { y: "0%" }, "-=0.5");
 
+// Animation slide
+  const pageTl = gsap.timeline()
     //Create scene
+    // pageTl:fromTo({slide, opacity:1, scale:1}, {opacity:0, scale:0});
     
-
-
 });
+
+
+
+
+gsap.utils.toArray(".slide").forEach((slide, i) => {
+  ScrollTrigger.create({
+    trigger: slide,
+    start: "top top", 
+    pin: true, 
+    pinSpacing: false,
+    autoAlpha: 1,
+    ease: "expo.out",
+    scrub: false,
+  });
+});
+
+
+ScrollTrigger.create({
+  snap: {
+    snapTo: 1 / 2,
+    duration: 2
+  }
+});
+
+//  gsap.to(slides, {
+//    scrollTrigger: ".slides",
+//    duration: 1,
+//    opacity:0,
+//    scale:0,
+
+// });
+
+// CURSOR
+
+function cursor(e) {
+  mouse.style.top = e.pageY + 'px';
+  mouse.style.left = e.pageX + 'px';
+};
+
+function activeCursor(e) {
+  const item = e.target;
+  if (item.id === 'logo' || item.classList.contains("burger")) {
+    mouse.classList.add('nav-active');
+  } else {
+    mouse.classList.remove('nav-active');
+  }
+
+  if (item.classList.contains("explore")) {
+    mouse.classList.add("explore-active");
+    gsap.to(".title-swipe", 1, {y:'0%'});
+    mouseTxt.innerText = 'tap'
+  } else {
+    mouse.classList.remove("explore-active");
+    gsap.to(".title-swipe", 1, {y:'100%'});
+    mouseTxt.innerText = ''
+  }
+}
+
+window.addEventListener("mousemove", cursor);
+window.addEventListener("mouseover", activeCursor);
 
 
